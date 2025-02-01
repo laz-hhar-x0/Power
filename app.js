b4d313f6e29ff608be3484681d8de1449a3e743b
lazhar233 <lazhhar3@gmail.com>
2025-02-01 20:58:10 +0100

    welcom5

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000;
+const mongoose = require("mongoose")
+app.set('view engine', 'ejs')
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.sendFile("./views/welcom.html", {root: __dirname})
+//   res.sendFile("/welcom.ejs", {root: __dirname})
+res.render('welcom', { } )
 })
 
 app.listen(port, () => {
